013c252c6d250ffd47b49a46a9d39e7e200af040
aniekanessien <obinna.a.essien@gmail.com>
2025-07-11 12:12:12 -0400

    completed /auth/login flow
    •	/auth/login redirects to Auth0
    •	Auth0 redirects back to /auth/callback
    •	ID token is parsed, and email/name/avatar are extracted
    •	GetUserByEmail is called
    •	If user is missing → fallback to CreateUser
    •	Success response is returned with user info (or at least success message)

--- internal/service/user/service.go
+++ internal/service/user/service.go
@@ -3,6 +3,7 @@ package userservice
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/obi2na/petrel/internal/db/models"
@@ -22,33 +23,41 @@ func NewUserService(db *pgxpool.Pool) *UserService {
 
 func (s *UserService) GetOrCreateUser(ctx context.Context, email, name, avatarURL string) (*models.User, error) {
 	user, err := s.queries.GetUserByEmail(ctx, email)
-	// user retrieval successful
-	if err == nil {
-		// update last_login_at
-		if err := s.queries.UpdateLastLogin(ctx, user.ID); err != nil {
-			logger.With(ctx).Error("UpdateLastLogin query failed", zap.Error(err))
-		}
-		return &user, nil
-	}
 
-	// database query failed
-	if err != sql.ErrNoRows {
+	if err != nil {
+		// user does not exist in table. no rows found
+		if errors.Is(err, sql.ErrNoRows) {
+			logger.With(ctx).Info("user does not exist. creating new user",
+				zap.String("email", email),
+			)
+			// create new user
+			newUserParams := models.CreateUserParams{
+				Email:     email,
+				Name:      name,
+				AvatarUrl: pgtype.Text{String: avatarURL, Valid: avatarURL != ""},
+				Provider:  pgtype.Text{String: "petrel", Valid: avatarURL != ""},
+			}
+
+			newUser, err := s.queries.CreateUser(ctx, newUserParams)
+			if err != nil {
+				logger.With(ctx).Error("CreateUser query failed", zap.Error(err))
+				return nil, err
+			}
+			return &newUser, nil
+		}
+
 		logger.With(ctx).Error("GetUserByEmail query failed", zap.Error(err))
 		return nil, err
 	}
 
-	// create new user
-	newUserParams := models.CreateUserParams{
-		Email:     email,
-		Name:      name,
-		AvatarUrl: pgtype.Text{String: avatarURL, Valid: avatarURL != ""},
-		Provider:  pgtype.Text{String: "petrel", Valid: avatarURL != ""},
-	}
+	logger.With(ctx).Info("user fetched from db",
+		zap.String("email", email),
+	)
 
-	newUser, err := s.queries.CreateUser(ctx, newUserParams)
-	if err != nil {
-		logger.With(ctx).Error("CreateUser query failed", zap.Error(err))
-		return nil, err
+	// user retrieval successful. update last_login_at
+	if err := s.queries.UpdateLastLogin(ctx, user.ID); err != nil {
+		logger.With(ctx).Error("UpdateLastLogin query failed", zap.Error(err))
 	}
-	return &newUser, nil
+	return &user, nil
+
 }
